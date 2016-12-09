2bf62d51580485708d81d059ec6d144afacb64b3
Matthew Swan <matthew.swan@raise.com>
2016-12-08 21:24:16 -0600

    add chapter 1, hello, world and chapter 2, simple string and arithmatic work

---
+++ chapter2/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("The length of the string Hello, World is:", len("Hello, World"))
+	fmt.Println("The second character of the string Hello, World is:", "Hello, World"[1])
+	fmt.Println("The 2 strings concatenated together make:", "Hello, "+"World")
+	fmt.Println("32132 * 42452 =", 32132*42452)
+}
---
+++ chapter2/multiplication.go
@@ -0,0 +1,7 @@
+package multiplication
+
+import "fmt"
+
+func main() {
+	fmt.Println("32132 * 45453 =", 32132*42452)
+}
