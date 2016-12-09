e0f49d43fba73d522d14477b3fabd5f75cd7c50e
Matthew Swan <matthew.swan@raise.com>
2016-12-08 22:00:47 -0600

    add chapter 4

---
+++ chapter3/fahr_to_cel/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Enter a number: ")
+	var input float64
+	fmt.Scanf("%f", &input)
+
+	fmt.Println("The Celsius degree is:", input)
+	output := ((input - 32) * 5 / 9)
+	fmt.Println("The Fahrenheit degree is:", output)
+}
