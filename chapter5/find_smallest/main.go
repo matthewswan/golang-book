27c1037753847c7c042949a8de4fe865ff145ec3
Matthew Swan <matthew.swan@raise.com>
2016-12-14 01:03:06 -0600

    add chapter5

---
+++ chapter5/find_smallest/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := []int{
+		48, 96, 86, 68,
+		57, 82, 63, 70,
+		37, 34, 83, 27,
+		19, 97, 9, 17,
+	}
+
+	smallest := x[0]
+	for i := 1; i < len(x); i++ {
+		if smallest < x[i] {
+			continue
+		} else {
+			smallest = x[i]
+		}
+	}
+	fmt.Println("The smallest value is:", smallest)
+}
